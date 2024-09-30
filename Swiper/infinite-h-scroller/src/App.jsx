430b37e350ab6541e2db9b0a4298a9f1d16fd613
Hilton-J <140448818+Hilton-J@users.noreply.github.com>
2024-09-30 12:43:49 +0200

    Update

--- Swiper/infinite-h-scroller/src/App.jsx
+++ Swiper/infinite-h-scroller/src/App.jsx
@@ -1,13 +1,14 @@
 // import Skills from './components/infinite'
 import './App.css'
-import SwiperComponent from './components/SwiperComponent'
+// import SwiperComponent from './components/SwiperComponent'
+import Infinite from './components/infinite'
 
 function App() {
 
   return (
     <>
       {/* <Skills /> */}
-      <SwiperComponent/>
+      <Infinite />
     </>
   )
 }
